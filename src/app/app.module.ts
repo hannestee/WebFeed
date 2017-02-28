17ed5df506ae06aaefe91cd89c7a29edfc99d6b1
spongefin <hannest@ety-a2123b-09.edu.wks.metropolia.fi>
2017-02-28 11:27:15 +0200

    previous video + fading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { VideoComponent } from './video/video.component';
 import {YoutubeService} from "./services/youtube.service";
 import { TrusturlPipe } from './pipes/trusturl.pipe';
+import { FaderComponent } from './fader/fader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoComponent,
-    TrusturlPipe
+    TrusturlPipe,
+    FaderComponent
   ],
   imports: [
     BrowserModule,
