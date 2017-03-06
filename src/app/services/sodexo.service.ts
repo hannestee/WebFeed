1c7c3b817d9d82631538beff9377c3de52c60900
spongefin <hannest@ety-a2123b-09.edu.wks.metropolia.fi>
2017-03-06 14:29:00 +0200

    rss

--- src/app/services/sodexo.service.ts
+++ src/app/services/sodexo.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
 import {Http, Headers, RequestOptions} from "@angular/http";
 import 'rxjs/add/operator/map';
+import {SodexoComponent} from "../sodexo/sodexo.component";
+
+//rss-service
 
 @Injectable()
 export class SodexoService {
 
+
+
   constructor(private http: Http) { }
 
   private rssToJson: string = ' https://api.rss2json.com/v1/api.json?rss_url='
-  private url: string = 'http://www.iltalehti.fi/rss/rss.xml';
+  private url: string = 'http://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss';
+  public dataupdated: EventEmitter<any> = new EventEmitter();
+  public data: any = [];
+
+  public setUrl (url) {
+    this.url = url;
+    console.log(this.url);
+    //this.rss.ngOnInit();
+  }
+
 
   public getActivities = () => {
     const headers = new Headers({'Content-Type': 'application/json'});
@@ -16,6 +30,14 @@ export class SodexoService {
     return this.http.get(this.rssToJson + this.url, options)
       .map(response => response.json());
   };
+
+  public setData = (data) => {
+    this.data = data;
+    this.dataupdated.emit(this.data);
+    //console.log(this.data);
+  };
+
+
 
 
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
 import {Http, URLSearchParams} from "@angular/http";
 import 'rxjs/add/operator/map';
 
@@ -11,11 +11,14 @@ export class YoutubeService {
 
   private url_activities: string = 'https://www.googleapis.com/youtube/v3/activities';
   private url_subs: string = 'https://www.googleapis.com/youtube/v3/subscriptions';
+  private url_channels: string = 'https://www.googleapis.com/youtube/v3/channels';
   private API_KEY: string = 'AIzaSyASRHawVhbWxXn4ha7Dg3phpLqTxknpyF8';
   private data: any = [];
   channelId: string = 'UCakgsb0w7QB0VHdnCc-OVEA'; //default
   pageToken: string = '';
-  private userId: string = 'UCQOn-y2tdd12TWwvhTn2AtA';
+  userId: string = 'UCQOn-y2tdd12TWwvhTn2AtA';
+  public nameupdated: EventEmitter<any> = new EventEmitter();
+  userName: string = 'test';
 
   public getActivities(){
     let params: URLSearchParams = new URLSearchParams();
@@ -40,6 +43,13 @@ export class YoutubeService {
       search: params
     }).map(response => response.json())
   };
+
+  public setData = (data) => {
+    this.data = data;
+    this.nameupdated.emit(this.data);
+    //console.log(this.data);
+  };
+
 
 
 
